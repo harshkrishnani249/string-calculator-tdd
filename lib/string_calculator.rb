218cf53be5667f5dbd159d114b1f8885bcf9a859
HarshKTntra <harsh.krishnani@tntra.io>
2025-03-08 15:07:15 +0530

    Support multiple numbers in StringCalculator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,8 +5,6 @@ module StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    num_array = numbers.split(",")
-    return num_array[0].to_i if num_array.size == 1
-    return num_array[0].to_i + num_array[1].to_i if num_array.size == 2
+    numbers.split(",").map(&:to_i).sum
   end
 end
